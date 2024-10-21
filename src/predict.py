0cb95e67b37b876ef85d317878400e3d2b2196c4
eby99 <ebymathew619@gmail.com>
2024-10-21 23:09:00 +0530

    Updated

--- src/predict.py
+++ src/predict.py
@@ -1,109 +1,75 @@
-"""
-    Script for single prediction on an image. It puts result in the folder.
-"""
-
-import argparse
 import os
 import random
-
-import matplotlib.pyplot as plt
 import numpy as np
 import torch
-
 from PIL import Image
+import matplotlib.pyplot as plt
+import streamlit as st
 
 from model import Net
 from utils import ConfigL, ConfigS, download_weights
 
-parser = argparse.ArgumentParser()
+# Streamlit application
+st.title("Image Caption Generator")
 
-parser.add_argument(
-    "-C", "--checkpoint-name", type=str, default="model.pt", help="Checkpoint name"
-)
+# Upload image file
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
 
-parser.add_argument(
-    "-S",
-    "--size",
-    type=str,
-    default="S",
-    help="Model size [S, L]",
-    choices=["S", "L", "s", "l"],
-)
+    # Model configuration
+    model_size = st.selectbox("Select Model Size", options=["S", "L"])
+    # temperature = st.slider("Select Temperature", min_value=0.0, max_value=2.0, value=1.0)
 
-parser.add_argument("-I", "--img-path", type=str, default="", help="Path to the image")
+    if st.button("Generate Caption"):
+        config = ConfigL() if model_size.upper() == "L" else ConfigS()
 
-parser.add_argument(
-    "-R",
-    "--res-path",
-    type=str,
-    default="./data/result/prediction",
-    help="Path to the results folder",
-)
+        # Set seed
+        random.seed(config.seed)
+        np.random.seed(config.seed)
+        torch.manual_seed(config.seed)
+        torch.cuda.manual_seed(config.seed)
+        torch.backends.cudnn.deterministic = True
 
-parser.add_argument(
-    "-T", "--temperature", type=float, default=1.0, help="Temperature for sampling"
-)
+        is_cuda = torch.cuda.is_available()
+        device = "cuda" if is_cuda else "cpu"
 
-args = parser.parse_args()
+        ckp_path = os.path.join(config.weights_dir, "model.pt")
 
-config = ConfigL() if args.size.upper() == "L" else ConfigS()
+        if not os.path.isfile(ckp_path):
+            download_weights(ckp_path, model_size)
 
-# set seed
-random.seed(config.seed)
-np.random.seed(config.seed)
-torch.manual_seed(config.seed)
-torch.cuda.manual_seed(config.seed)
-torch.backends.cudnn.deterministic = True
+        model = Net(
+            clip_model=config.clip_model,
+            text_model=config.text_model,
+            ep_len=config.ep_len,
+            num_layers=config.num_layers,
+            n_heads=config.n_heads,
+            forward_expansion=config.forward_expansion,
+            dropout=config.dropout,
+            max_len=config.max_len,
+            device=device,
+        )
 
-is_cuda = torch.cuda.is_available()
-device = "cuda" if is_cuda else "cpu"
+        checkpoint = torch.load(ckp_path, map_location=device)
+        model.load_state_dict(checkpoint)
+        model.eval()
 
-if __name__ == "__main__":
-    ckp_path = os.path.join(config.weights_dir, args.checkpoint_name)
+        with torch.no_grad():
+            caption, _ = model(img)
 
-    assert os.path.isfile(args.img_path), "Image does not exist"
+        st.write(f'Generated Caption: "{caption}"')
 
-    if not os.path.exists(args.res_path):
-        os.makedirs(args.res_path)
+        # Save the image with the caption
+        plt.imshow(img)
+        plt.title(caption)
+        plt.axis("off")
 
-    img = Image.open(args.img_path)
+        img_save_path = f'{os.path.splitext(uploaded_file.name)[0]}-captioned.jpg'
+        plt.savefig(img_save_path, bbox_inches="tight")
+        plt.clf()
+        plt.close()
 
-    model = Net(
-        clip_model=config.clip_model,
-        text_model=config.text_model,
-        ep_len=config.ep_len,
-        num_layers=config.num_layers,
-        n_heads=config.n_heads,
-        forward_expansion=config.forward_expansion,
-        dropout=config.dropout,
-        max_len=config.max_len,
-        device=device,
-    )
-
-    if not os.path.exists(config.weights_dir):
-        os.makedirs(config.weights_dir)
-
-    if not os.path.isfile(ckp_path):
-        download_weights(ckp_path, args.size)
-
-    checkpoint = torch.load(ckp_path, map_location=device)
-    model.load_state_dict(checkpoint)
-
-    model.eval()
-
-    with torch.no_grad():
-        caption, _ = model(img, args.temperature)
-
-    plt.imshow(img)
-    plt.title(caption)
-    plt.axis("off")
-
-    img_save_path = (
-        f'{os.path.split(args.img_path)[-1].split(".")[0]}-R{args.size.upper()}.jpg'
-    )
-    plt.savefig(os.path.join(args.res_path, img_save_path), bbox_inches="tight")
-
-    plt.clf()
-    plt.close()
-
-    print('Generated Caption: "{}"'.format(caption))
+        # Display the saved image
+        st.image(img_save_path, caption='Captioned Image.', use_column_width=True)
