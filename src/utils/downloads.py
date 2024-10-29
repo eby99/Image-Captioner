c262c899e02fae01d5f899c0e27c74a7f1635aab
eby99 <148738516+eby99@users.noreply.github.com>
2024-10-29 22:34:35 +0530

    Update downloads.py

--- src/utils/downloads.py
+++ src/utils/downloads.py
@@ -1,7 +1,4 @@
-"""
-    Utility functions for loading weights.
-"""
-
+import os
 import gdown
 
 MODEL_WEIGHTS = {
@@ -9,14 +6,15 @@ MODEL_WEIGHTS = {
     "S": "1pSQruQyg8KJq6VmzhMLFbT_VaHJMdlWF",
 }
 
-
 def download_weights(checkpoint_fpath, model_size="L"):
     """
     Downloads weights from Google Drive.
     """
+    if os.path.exists(checkpoint_fpath):
+        print(f"File {checkpoint_fpath} already exists. Skipping download.")
+        return
 
     download_id = MODEL_WEIGHTS[model_size.strip().upper()]
-
     gdown.download(
         f"https://drive.google.com/uc?id={download_id}", checkpoint_fpath, quiet=False
     )
